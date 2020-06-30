30ed0d272233fef57229327307df860bfdc10382
jreuschenbach <j.reuschenbach@onOffice.com>
2020-06-30 21:06:03 +0200

    invalid-case
     * schema extended / required properties on top-level
     * test-case extended

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -5,12 +5,20 @@ use jr\ooapigenerator\JsonValidator;
 
 class SchemaTest extends TestCase
 {
-    public function testReadAddress(): void
+    public function testValid(): void
     {
         $schema = Opis\JsonSchema\Schema::fromJsonString(file_get_contents(__DIR__ . '/../resource/onOffice-api-json-schema.json'));
         $validator = new \Opis\JsonSchema\Validator();
         $validationResult = $validator->schemaValidation(json_decode(file_get_contents(__DIR__ . '/../resource/example-read-address.json')), $schema);
         $this->assertFalse($validationResult->hasErrors(), $this->extractErrorInfos($validationResult));
+    }
+
+    public function testInvalid(): void
+    {
+        $schema = \Opis\JsonSchema\Schema::fromJsonString(file_get_contents(__DIR__.'/../resource/onOffice-api-json-schema.json'));
+        $validator = new \Opis\JsonSchema\Validator();
+        $validationResult = $validator->schemaValidation(json_decode(file_get_contents(__DIR__.'/../resource/example-invalid-only-action.json')), $schema);
+        $this->assertTrue($validationResult->hasErrors());
     }
 
     private function extractErrorInfos(\Opis\JsonSchema\ValidationResult $validationResult): string
